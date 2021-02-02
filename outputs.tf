56baa0dd414d4d31510f6d9fb491807a5830bf9e
Petr Lebedev <lebedevpetr@outlook.com>
2021-02-02 20:25:06 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "ip" {
   description = "piblic ip address"
-  value = azurerm_public_ip.main
+  value = azurerm_public_ip.main.ip_address
 }
 output "domain" {
   description = "public domain"
