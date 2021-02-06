71f72b26a97d8f9aa882ac47e0d25c505199fcbc
Petr Lebedev <lebedevpetr@outlook.com>
2021-02-06 13:41:19 +0000

    Update .gitlab-ci.yml, terraform/main.tf, docker/traefik/traefik.yml, docker/docker-compose.yml files

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,6 +71,17 @@ resource "azurerm_network_security_group" "main" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+    security_rule {
+    name                       = "traefik"
+    priority                   = 103
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "9000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
   security_rule {
     name                       = "https"
     priority                   = 103
